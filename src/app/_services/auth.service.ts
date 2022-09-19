141f33a56511708ebaaef36dd9cf6373d1746404
Luis Mariano Bibbo <lmbibbo@gmail.com>
2022-09-19 08:44:04 -0300

    Pequeños Cambios

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators/map';
 
 const AUTH_API = 'http://192.168.0.21:8080/';
 
@@ -17,11 +18,14 @@ export class AuthService {
   login(username: string, password: string): Observable<any> {
     let cuerpo = JSON.parse('{"username":"linda", "password":"password"}')
     console.log(AUTH_API + 'login');
-    return this.http.post(
-      AUTH_API + 'login', cuerpo, httpOptions
-    );
+    return this.http.post(AUTH_API + 'login', cuerpo, httpOptions).pipe(
+      map((data: Response) => {
+        console.log(data);
+        return data;
+      }
+    ));
   }
-
+  
   register(username: string, email: string, password: string): Observable<any> {
     return this.http.post(
       AUTH_API + 'signup',
